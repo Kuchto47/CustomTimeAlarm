08c0ca95730ddb2e6558fd5083464f0cfd74e0d0
jurajvandor <433451@mail.muni.cz>
2018-03-28 19:59:34 +0200

    main activity layout (should be changed to main fragment) created settings layout created (snooze time not working - crashes)

---
+++ app/src/main/java/com/project/pv239/customtimealarm/Fragments/SettingsFragment.java
@@ -0,0 +1,22 @@
+package com.project.pv239.customtimealarm.Fragments;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import com.project.pv239.customtimealarm.R;
+public class SettingsFragment
+        extends PreferenceFragment {
+
+    @NonNull
+    public static SettingsFragment newInstance() {
+        return new SettingsFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.settings);
+    }
+}
