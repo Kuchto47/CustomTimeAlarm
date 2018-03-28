eed16220c5f5abddefc880f8b2283140d401c5f3
Martin <martin.k738@gmail.com>
2018-03-28 19:29:37 +0200

    added dummy implementation of TimeHelper class and changed little functionality of some private methods in QueryProcessor

--- app/src/main/java/com/project/pv239/customtimealarm/api/GoogleMapsApi.java
+++ app/src/main/java/com/project/pv239/customtimealarm/api/GoogleMapsApi.java
@@ -3,9 +3,10 @@ package com.project.pv239.customtimealarm.api;
 import com.project.pv239.customtimealarm.helpers.Query.QueryProcessor;
 
 public class GoogleMapsApi {
-    public void getDirections() {
+    public int getTimeOfTravelInSeconds() {
         QueryProcessor queryProcessor = new QueryProcessor();
         String query = queryProcessor.getQuery();
-        //
+        //TODO process JSON response and return seconds as int
+        return 1234;
     }
 }
