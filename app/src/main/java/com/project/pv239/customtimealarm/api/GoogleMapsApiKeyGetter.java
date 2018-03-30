aad8a4c54c25ebb6df5262942ff9d36a987f9086
Martin <martin.k738@gmail.com>
2018-03-30 11:43:03 +0200

    delete conflicted class

--- app/src/main/java/com/project/pv239/customtimealarm/api/GoogleMapsApiKeyGetter.java
+++
@@ -1,9 +0,0 @@
-package com.project.pv239.customtimealarm.api;
-
-public class GoogleMapsApiKeyGetter {
-    private static final String apiKey = "AIzaSyCHqgjw-K-ffk-m4eo78eWaag4YOSWGlAE";
-
-    public static String getApiKey(){
-        return apiKey;
-    }
-}
