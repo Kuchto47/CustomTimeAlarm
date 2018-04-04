8d5903ba8a68cb7331d7ea76fbc3135b530f5ee2
Martin <martin.k738@gmail.com>
2018-04-04 22:03:21 +0200

    static db name

--- app/src/main/java/com/project/pv239/customtimealarm/database/DatabaseProvider.java
+++ app/src/main/java/com/project/pv239/customtimealarm/database/DatabaseProvider.java
@@ -6,10 +6,11 @@ import com.project.pv239.customtimealarm.activities.MainActivity;
 
 public class DatabaseProvider {
     private static AppDatabase appDatabase = null;
+    private static String DB_NAME = "alarm.db";
 
     public static AppDatabase getDatabase(){
         if(appDatabase == null){
-            appDatabase = Room.databaseBuilder(MainActivity.context, AppDatabase.class, "alarm-database").build();
+            appDatabase = Room.databaseBuilder(MainActivity.context, AppDatabase.class, DB_NAME).build();
         }
         return appDatabase;
     }
