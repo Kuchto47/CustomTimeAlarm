7ccaf16885a03a13f88d506bd46684ef51862857
Martin <martin.k738@gmail.com>
2018-04-04 22:15:20 +0200

    on conflict straetgy in dao

--- app/src/main/java/com/project/pv239/customtimealarm/database/dao/AlarmDao.java
+++ app/src/main/java/com/project/pv239/customtimealarm/database/dao/AlarmDao.java
@@ -3,6 +3,7 @@ package com.project.pv239.customtimealarm.database.dao;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
 
 import com.project.pv239.customtimealarm.database.entity.Alarm;
@@ -14,7 +15,7 @@ public interface AlarmDao {
     @Query("SELECT * FROM alarm")
     List<Alarm> getAll();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void addAlarm(Alarm alarm);
 
     @Delete
