643c371f374fafb87e89cfba37a2de6788bef3cb
jurajvandor <433451@mail.muni.cz>
2018-05-01 15:23:04 +0200

    code refactor

--- app/src/main/java/com/project/pv239/customtimealarm/views/CustomScrollView.java
+++ app/src/main/java/com/project/pv239/customtimealarm/views/CustomScrollView.java
@@ -23,34 +23,22 @@ public class CustomScrollView extends ScrollView {
         final int action = ev.getAction();
         switch (action) {
             case MotionEvent.ACTION_DOWN:
-                //Log.i("CustomScrollView", "onInterceptTouchEvent: DOWN super false" );
                 super.onTouchEvent(ev);
                 break;
-
             case MotionEvent.ACTION_MOVE:
                 return false; // redirect MotionEvents to ourself
-
             case MotionEvent.ACTION_CANCEL:
-                // Log.i("CustomScrollView", "onInterceptTouchEvent: CANCEL super false" );
                 super.onTouchEvent(ev);
                 break;
-
             case MotionEvent.ACTION_UP:
-                //Log.i("CustomScrollView", "onInterceptTouchEvent: UP super false" );
                 return false;
-
-            default:
-                //Log.i("CustomScrollView", "onInterceptTouchEvent: " + action );
-                break;
         }
-
         return false;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent ev) {
         super.onTouchEvent(ev);
-        //Log.i("CustomScrollView", "onTouchEvent. action: " + ev.getAction() );
         return true;
     }
 }
