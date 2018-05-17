39112a461fcb4f28bbcce59ff78cc0826a9397e8
jurajvandor <433451@mail.muni.cz>
2018-05-17 18:28:27 +0200

    small fixes

--- app/src/main/java/com/project/pv239/customtimealarm/fragments/MainFragment.java
+++ app/src/main/java/com/project/pv239/customtimealarm/fragments/MainFragment.java
@@ -2,6 +2,7 @@ package com.project.pv239.customtimealarm.fragments;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -43,7 +44,7 @@ public class MainFragment extends Fragment{
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
         mUnbinder = ButterKnife.bind(this, view);
         mAdapter = new AlarmsAdapter(new ArrayList<Alarm>(), this);
